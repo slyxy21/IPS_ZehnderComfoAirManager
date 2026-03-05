e47b4370a3e2edd10180efc40344e61c8a38441d
slyxy21 <slyxy@gmx.de>
2026-03-05 17:27:33 +0100

    chore: initial project structure - Zehnder ComfoAir Manager

---
+++ README.md
@@ -0,0 +1,15 @@
+# Zehnder ComfoAir Manager
+
+IP-Symcon Modul für Zehnder ComfoAir Manager
+
+## Voraussetzungen
+- IP-Symcon 8.0+
+
+## Installation
+Module URL in IP-Symcon eintragen:
+\\\
+https://github.com/slyxy21/IPS_ZehnderComfoAirManager
+\\\
+
+## Autor
+slyxy21 <slyxy@gmx.de>
---
+++ ZehnderComfoAirManager/module.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+class ZehnderComfoAirManager extends IPSModule
+{
+    public function Create(): void
+    {
+        parent::Create();
+        // Eigenschaften registrieren
+    }
+
+    public function Destroy(): void
+    {
+        parent::Destroy();
+    }
+
+    public function ApplyChanges(): void
+    {
+        parent::ApplyChanges();
+    }
+}
